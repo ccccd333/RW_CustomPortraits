2638432a0f24ecd991a273b1a986a87b8f38de7b
ccccd333 <sanosukeyanplus3@gmail.com>
2025-08-08 23:39:01 +0900

    1.5更新。アニメーションの場合PNGでは重いのでDDS読み取りに変更。また、NullRefやOORエラーの修正。

---
+++ 1.5/Source/CustomPortraitsEx/PExSetting.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Foxy.CustomPortraits.CustomPortraitsEx
+{
+    public class PExSetting
+    {
+        [JsonProperty("display_duration")]
+        public float DisplayDuration { get; set; }
+    }
+}
--- 1.5/Source/CustomPortraitsEx/Refs/MoodRefs.cs
+++ 1.5/Source/CustomPortraitsEx/Refs/MoodRefs.cs
@@ -1,7 +1,10 @@
 ﻿
 using Foxy.CustomPortraits.CustomPortraitsEx.Repository;
+using RimWorld;
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Verse;
 
 namespace Foxy.CustomPortraits.CustomPortraitsEx
 {
@@ -11,6 +14,42 @@ namespace Foxy.CustomPortraits.CustomPortraitsEx
         public Dictionary<string, string> group_filter = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
         public Dictionary<string, PriorityWeights> priority_weights = new Dictionary<string, PriorityWeights>(StringComparer.OrdinalIgnoreCase);
         public string fallback_mood = "";
+        public string fallback_mood_on_death = "";
+
+        public Dictionary<string, Regex> txs_regex_cache = new Dictionary<string, Regex>(StringComparer.OrdinalIgnoreCase);
+        public Dictionary<string, Regex> g_regex_cache = new Dictionary<string, Regex>(StringComparer.OrdinalIgnoreCase);
+        public Dictionary<string, Regex> pw_regex_cache = new Dictionary<string, Regex>(StringComparer.OrdinalIgnoreCase);
+
+        public bool MatchDictKeysByRegex(string input, out string access_key)
+        {
+            access_key = "";
+
+            foreach(var tx in txs)
+            {
+                //Log.Message($"[PortraitsEx] MatchDictKeysByRegex key: {tx.Key} input: {input}");
+                if (txs_regex_cache.ContainsKey(tx.Key))
+                {
+                    var reg = txs_regex_cache[tx.Key];
+                    if (reg.IsMatch(input))
+                    {
+                        //Log.Message($"[PortraitsEx] MatchDictKeysByRegex pic ==> key: {tx.Key} input: {input}");
+                        access_key = tx.Key;
+                        return true;
+                    }
+                }
+                else
+                {
+                    if(tx.Key == input)
+                    {
+                        //Log.Message($"[PortraitsEx] MatchDictKeysByRegex pic ==> key: {tx.Key} input: {input}");
+                        access_key = tx.Key;
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
 
         public virtual bool contain(string key) { return false; }
     }
