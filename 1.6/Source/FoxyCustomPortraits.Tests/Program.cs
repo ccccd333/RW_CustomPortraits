8a399addd7049d6a21328b14cdb8a579f5bcdf39
ccccd333 <sanosukeyanplus3@gmail.com>
2026-08-22 23:27:46 +0900

    <バグ修正> ・RepeatEventのmin以下で同じコンテキストが来た時operationの評価がされていなかったので修正 ・RepeatEventのlast_context_nameの一つ前のコンテキストが非同期処理と同期処理の間でnullのタイミングがありエラーにはならないが、last_context_nameの動作に支障をきたしていたので修正。

--- 1.6/Source/FoxyCustomPortraits.Tests/Program.cs
+++ 1.6/Source/FoxyCustomPortraits.Tests/Program.cs
@@ -335,7 +335,10 @@ namespace FoxyCustomPortraits.Tests
 
             if (IsBelowMinCount(loop_settings, repeat_index, override_min_count))
             {
-                return portrait_context_name == bef_context_name ? 1 : 0;
+                if (portrait_context_name != bef_context_name)
+                {
+                    return 0;
+                }
             }
 
             if (IsAboveMaxCount(loop_settings, repeat_index, override_max_count))
